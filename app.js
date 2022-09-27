94db739924c45ff19146974900c858df251a58b9
Sam Skinner <30613658+sskii@users.noreply.github.com>
2022-09-27 21:21:29 +1300

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ async function inform() {
 
 	navigator.bluetooth.requestDevice()
 		.then(device => {
-		log('> Name:             ' + device.name);
-		log('> ID:               ' + device.id);
-		log('> Connected:        ' + device.gatt.connected);
-	})
-	.catch(error => {
-		log('Failed. ' + error);
-	});
+			log('> Name:             ' + device.name);
+			log('> ID:               ' + device.id);
+			log('> Connected:        ' + device.gatt.connected);
+		})
+		.catch(error => {
+			log('Failed. ' + error);
+		});
 
 	console.log(available);
 
